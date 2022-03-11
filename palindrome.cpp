7af547996aa8270f806ea45177ada94ce9bcffc6
Orchismita10 <99389659+Orchismita10@users.noreply.github.com>
2022-03-11 10:50:39 +0530

    Create palindrome.cpp
    Check if a number is palindrome or not

---
+++ palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number to be checked:";
+    cin>>n;
+    int num=n, digit, rev=0;
+    do{
+        digit= num%10;
+        rev = (rev*10)+digit;
+        num = num/10;
+    } while(num != 0);
+    if(rev ==n){
+        cout<< n << " is Paindrome.";
+    }
+    else{
+        cout<< n << " is not Palindrome.";
+    }
+
+    return 0;
+}
