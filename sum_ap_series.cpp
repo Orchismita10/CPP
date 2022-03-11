974ea4ce9ee9b678fb61ebc977b317ec27d9b99a
Orchismita10 <99389659+Orchismita10@users.noreply.github.com>
2022-03-11 15:01:48 +0530

    Create sum_ap_series.cpp
    Find Sum of AP Series

---
+++ sum_ap_series.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double a;
+    cout<<"Enter the first term:";
+    cin>>a;
+    double d;
+    cout<<"Enter the common difference:";
+    cin>>d;
+    int n;
+    cout<<"Enter the number of terms:";
+    cin>>n;
+    double sum = (n / 2.0) * (2.0 * a + (n - 1) * d);
+    cout<< "Sum of Given Ap Series: "<< sum;
+    
+    return 0;
+}
