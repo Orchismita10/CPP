fd12f7232fd6dbf19bfd064dae91489d1100e45a
Orchismita10 <99389659+Orchismita10@users.noreply.github.com>
2022-03-11 14:10:20 +0530

    Create armstrong.cpp
    Check if a number is armstrong number of not

---
+++ armstrong.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number to be checked:";
+    cin >> n;
+    int count=0, power, digit, r, sum=0;
+    int num = n;
+    while( num!=0 )
+    {
+        num = num/10;
+        count++;
+    }
+    power=count;
+    while(num>0)
+    {
+        digit = num%10;
+        r = pow(digit,power);
+        sum = sum+r;
+        num= num/10;
+    }
+    if( sum == n )
+    {
+        cout << n << " is an Armstrong number.";
+    }
+    else
+    {
+        cout << n << " is not an Armstrong number.";
+    }
+
+    return 0;
+}
