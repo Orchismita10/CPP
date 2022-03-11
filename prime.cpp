d1f5996f3b6881d8d04499f9c1aca50453ad947b
Orchismita10 <99389659+Orchismita10@users.noreply.github.com>
2022-03-11 12:53:24 +0530

    Create prime.cpp
    Check if a number is prime or not

---
+++ prime.cpp
@@ -0,0 +1,22 @@
+int main()
+{
+    int num;
+    cout << "Enter the number to be checked:";
+    cin >> num;
+    int temp=0;
+    for(int i=2; i<=(num/2); i++)
+    {
+        if(num%i==0)
+        {
+            cout << num << " is not a prime number.";
+            temp=1;
+            break;
+        }
+    }
+    if( temp == 0)
+    {
+         cout << num << " is Prime." << endl; 
+    }
+
+    return 0;
+}
